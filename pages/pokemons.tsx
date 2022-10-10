88b4af2951490dd6022d6c2b32d07184b6e50b82
Phat2222222 <letienphat2207@gmail.com>
2022-10-10 21:30:00 +0700

    create layout for pokemons grid

--- pages/pokemons.tsx
+++ pages/pokemons.tsx
@@ -2,15 +2,29 @@ import Image from "next/image";
 
 export default function Test() {
   return (
-    <>
+    <div className="container mx-auto px-10 pb-20 ">
       <div className="grid place-items-center py-20 ">
         <Image src="/pokemon.png" alt="pokemon-logo" width={400} height={150} />
       </div>
-      <div className=" grid grid-rows-4 grid-flow-col gap-4">
+
+      <div className="grid grid-flow-row grid-cols-auto gap-12">
         {new Array(20).fill(null).map((_, index) => (
-          <div className="w-64 h-64 bg-yellow-200 rounded-lg" key={index}>a</div>
+          <div
+            className="relative w-full pt-[100%] bg-yellow-200 rounded-lg text-black text-2xl"
+            key={index}
+          >
+            <div className="absolute top-0 left-0 grid place-items-center w-full h-full">
+              <Image
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+                  index + 1
+                }.svg`}
+                alt="pokemon"
+                layout="fill"
+              />
+            </div>
+          </div>
         ))}
       </div>
-    </>
+    </div>
   );
 }
